8d11f33680926c40343e8b381df57be7cb6071cf
VanillaWulf <annarsve@gmail.com>
2018-03-10 14:46:30 +0500

    tracklist change

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -3,19 +3,20 @@ import './TrackList.css';
 import Track from '../Track/Track.js'
 
 class TrackList extends Component{
-    render(){
-        return(
+  render(){
+    return(
       <div className="TrackList">
-      {
-        this.props.tracks.map(track => {
-          return (<Track track={ track }
-                      key={ track.id }
-                      onAdd={ this.props.onAdd }
-                      onRemove={ this.props.onRemove }
-                      isRemoval={this.props.isRemoval}/>
-                  )
-        })
-      }
+        {
+          this.props.tracks.map(track => {
+            return (
+              <Track track={ track }
+              key={ track.id }
+              onAdd={ this.props.onAdd }
+              onRemove={ this.props.onRemove }
+              isRemoval={this.props.isRemoval} />
+              )
+            })
+        }
       </div>
     );
   }
